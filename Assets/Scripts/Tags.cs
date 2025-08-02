f9eb655f153967f33b080a8a793f695c32b65114
faizahsayyid <faizah.sayyid@gmail.com>
2025-08-02 19:46:49 -0400

    feat: camera follow

--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -1,5 +1,6 @@
 sealed public class Tags
 { 
+    public const string Player = "Player";
     public const string SceneLeftExit = "SceneLeftExit";
     public const string SceneRightExit = "SceneRightExit";
 }
